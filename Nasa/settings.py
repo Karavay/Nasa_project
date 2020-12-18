0340c4a9d25583114ef45465d9fea60130540137
Ilya <ilia.karavay@gmail.com>
2020-12-19 00:07:36 +0300

    change the db

--- Nasa/settings.py
+++ Nasa/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'Nasa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Nasa',
-        'USER': 'ilya',
-        'PASSWORD': 'ipihaf473428',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
